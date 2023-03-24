49711664385aded3e94fb5a834b1c7a08d6d80dc
Mrigakshi Roy Choudhury <mrigakshirc@gmail.com>
2023-03-24 21:23:26 +0530

    added style to the header

--- src/components/Main Header/MainHeader.js
+++ src/components/Main Header/MainHeader.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './MainHeader.module.css';
 
 const MainHeader = () => {
   return (
-    <header>
+    <header className={classes['main-header']}>
       <h1>Blogs Page</h1>
     </header>
   );
