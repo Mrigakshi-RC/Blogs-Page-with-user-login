14e1038e676e9534a1f9f74943856a40eaf79b3e
Mrigakshi Roy Choudhury <mrigakshirc@gmail.com>
2023-03-24 21:47:33 +0530

    added validation styles for email input

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,38 @@
 import React, { useState, useEffect, useReducer } from 'react';
 import classes from './Login.module.css';
 
+const emailReducer = (state,action) => {
+  if (action.type === "USER_INPUT"){
+    return {value: action.val, isValid:action.val.includes('@') && action.val.includes('.')}
+  }
+  return { value:"", isValid: false};
+};
+
 const Login = () => {
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value:"",
+    isValid: undefined,
+  });
+
+
+  const emailChangeHandler = (event) => {
+    dispatchEmail({type: "USER_INPUT", val: event.target.value});
+  }
+  
   return (
     <div className={`${classes.login} ${classes.card}`}>
       <form>
         <div
-          className={classes.control}
+          className={`${classes.control} ${
+            emailState.isValid === false ? classes.invalid : ''
+          }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
+            value={emailState.value}
+            onChange={emailChangeHandler}
           />
         </div>
         <div
