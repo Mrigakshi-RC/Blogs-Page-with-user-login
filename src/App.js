9bc29d7921d8a5b068371001fb3554820a0a0b65
Mrigakshi Roy Choudhury <mrigakshirc@gmail.com>
2023-03-24 21:15:53 +0530

    placed the header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react"
-import Login from "./components/Login";
+import Login from "./components/Login/Login";
+import MainHeader from "./components/Main Header/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn]=useState(false);
   return (
     <React.Fragment>
+      <MainHeader/>
       <main>
         {!isLoggedIn && <Login/>}
       </main>      
---
+++ src/components/Main Header/MainHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainHeader = () => {
+  return (
+    <header>
+      <h1>Blogs Page</h1>
+    </header>
+  );
+};
+
+export default MainHeader;
